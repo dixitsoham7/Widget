d3ed8c89131dc4d587534e9d0d92edbec31e718e
dmkdfc <sohammalhar@gmail.com>
2018-04-30 23:20:22 +0530

    Initial commit

---
+++ app/src/main/java/com/srm/sohamdixit/homewidget/Widget.java
@@ -0,0 +1,37 @@
+package com.srm.sohamdixit.homewidget;
+
+/**
+ * Created by Soham Dixit on 30-04-2018.
+ */
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_design);
+
+        // Create an Intent object includes my website address
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.acadgild.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        //handle click event of the TextView (launch browser and go to my website)
+        views.setOnClickPendingIntent(R.id.txtWidget, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
